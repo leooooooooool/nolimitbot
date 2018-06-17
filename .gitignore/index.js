29b712095d2285e39d15251d051677a2081284b7
leooooooooool <38082818+leooooooooool@users.noreply.github.com>
2018-06-17 11:50:47 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,24 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
+const PREFIX = "!";
+
+
+bot.on('ready', function() {
+    bot.user.setActivity('rien')
+    console.log("I'm Ready !");
+});
+
+
+
+
+bot.on('guildMemberAdd', member => {
+    member.guild.channels.find("name", "logs").send(` ${member} a reçu le role [Nouveaux]`)
+    var role = member.guild.roles.find('name', '[Nouveaux]');
+    member.addRole(role)
+    
+});
+
+
+
+bot.login(process.env.TOKEN)
